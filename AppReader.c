8ba14aca3d3efd2648bda0028b1503f95c425bd3
Thomm <tbellp@gmail.com>
2017-04-03 23:08:36 -0300

    Create AppReader.c

---
+++ AppReader.c
@@ -0,0 +1,7 @@
+//
+//  AppReader.c
+//  Reader data from smartphones wirelessly & no client app
+//
+//  Created by Thomas Bell on 4/03/17.
+//  Copyright (c) 2017 Thomas Bell. All rights reserved.
+//
