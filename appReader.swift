5ca7cf9b4dbab48840e97c841ba51da23466b527
Thomm <tbellp@gmail.com>
2017-04-05 02:07:02 -0300

    Update appReader.swift

--- appReader.swift
+++ appReader.swift
@@ -1,6 +1,6 @@
 //
 //  appReader.swift
-//  Reader data from smartphones wirelessly & no client app
+//  Reader data from smartphones wirelessly
 //
 //  Created by Thomas Bell on 4/03/17.
 //  Copyright (c) 2017 Thomas Bell. All rights reserved.
